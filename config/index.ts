365282b1e21753fb1914e4eac7289b76cc90cc1f
janakhpon <jnovaxer@gmail.com>
2020-06-08 19:13:46 +0630

    getting Denosaurs ready

---
+++ config/index.ts
@@ -0,0 +1,9 @@
+const DBCreds = {
+    user: "testuser",
+    database: "testDB",
+    password: "testpassword",
+    hostname: "localhost",
+    port: 5432
+}
+
+export { DBCreds }
---
+++ interfaces/index.ts
@@ -0,0 +1,9 @@
+export interface Dinosaur {
+    id: string;
+    name: string;
+    description: string;
+    weight: number;
+    length: number;
+    type: string;
+    environment: string;
+}
